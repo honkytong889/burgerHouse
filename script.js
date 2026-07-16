b49e94cbc66d3691bc10d00a97e4011a6df72777
stefan <s.friggemann@t-online.de>
2026-07-16 09:23:48 +0200

    added renderEmptyBasket function

--- script.js
+++ script.js
@@ -24,3 +24,25 @@ function renderBasket() {
     (dishesInBasket.dishes.length == 0) ? renderEmptyBasket() : renderFilledBasket();
 }
 
+
+function renderEmptyBasket() {
+
+    const shoppingCartRef = document.getElementById('ShoppingCart');
+    if (shoppingCartRef) shoppingCartRef.classList.remove("dishes-added-to-basket");
+
+    const amountDishesRef = document.getElementById('AmountDishes');
+    if (amountDishesRef) amountDishesRef.classList.add("d-none");
+
+    const mobileAmountRef = document.getElementById('MobileBasketAmount');
+    if (mobileAmountRef) {
+        mobileAmountRef.classList.add("d-none");
+        mobileAmountRef.innerHTML = "0";
+    }
+
+    const basketAsideRef = document.getElementById('BasketAside');
+    if (basketAsideRef) basketAsideRef.innerHTML = templateEmptyBasket();
+
+    const basketDialogRef = document.getElementById('BasketDialog');
+    if (basketDialogRef) basketDialogRef.innerHTML = templateEmptyBasket();
+}
+
